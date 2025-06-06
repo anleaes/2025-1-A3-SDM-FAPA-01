468d6e272bbe68b06d7c82f8ac06400085a6235b
pedro-rohr <pedrorohr04@gmail.com>
2025-06-06 20:26:59 -0300

    Views dos movimentos

--- apps/movements/views.py
+++ apps/movements/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Movement
+from rest_framework import viewsets
+from .serializer import MovementSerializer
+
+class MovementViewSet(viewsets.ModelViewSet):
+    queryset = Movement.objects.all()
+    serializer_class = MovementSerializer  
