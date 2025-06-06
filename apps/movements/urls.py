ac017220fbd5bea16792ef91310c9178d1513d9d
pedro-rohr <pedrorohr04@gmail.com>
2025-06-06 20:30:06 -0300

    Adição de URLs para o app movements

--- apps/movements/urls.py
+++ apps/movements/urls.py
@@ -4,3 +4,9 @@ from rest_framework import routers
 
 app_name = 'movements'
 
+router = routers.DefaultRouter()
+router.register('', views.MovementViewSet, basename='movements')
+
+urlpatterns = [
+    path('', include(router.urls) )
+]
