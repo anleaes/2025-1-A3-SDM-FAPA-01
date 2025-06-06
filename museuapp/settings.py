ee22afc555de6ddd961674e901255c477f92ac06
pedro-rohr <pedrorohr04@gmail.com>
2025-06-06 20:39:39 -0300

    Debug da classe movimentos no banco

--- museuapp/settings.py
+++ museuapp/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'movements.apps.MovimentosConfig',
+    'movements.apps.MovementsConfig',
 ]
 
 MIDDLEWARE = [
