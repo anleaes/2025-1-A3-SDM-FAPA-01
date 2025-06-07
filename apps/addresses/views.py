db72845c591ab2355f310011b66025b447a20281
pedro-rohr <pedrorohr04@gmail.com>
2025-06-06 21:25:04 -0300

    Criação ViewSet

--- apps/addresses/views.py
+++ apps/addresses/views.py
@@ -6,3 +6,6 @@ from .models import Address
 from rest_framework import viewsets
 from .serializers import AddressSerializer
 
+class AddressViewSet(viewsets.ModelViewSet):
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer  
