40437db9d82e371708b574595d0686e8b93ad4c5
pedro-rohr <pedrorohr04@gmail.com>
2025-06-18 20:50:42 -0300

    Adição dos serializadores (ainda não implementados)

--- apps/museums/views.py
+++ apps/museums/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework import viewsets
 from .models import Museum
-
+from .serializers import MuseumSerializer
 
 class MuseumViewSet(viewsets.ModelViewSet):
     queryset = Museum.objects.all()
+    serializer_class = MuseumSerializer
     
