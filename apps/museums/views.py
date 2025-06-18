ac0c0ea53ddc6f9efdd467ba094dd55203c28e09
pedro-rohr <pedrorohr04@gmail.com>
2025-06-18 20:50:10 -0300

    Implementação views

--- apps/museums/views.py
+++ apps/museums/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .models import Museum
+
+
+class MuseumViewSet(viewsets.ModelViewSet):
+    queryset = Museum.objects.all()
+    
