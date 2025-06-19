4d1183cd83059af498691adf02609140dfaa3ada
pedro-rohr <pedrorohr04@gmail.com>
2025-06-19 00:44:44 -0300

    Implementação do views

--- apps/events/views.py
+++ apps/events/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Event
+from rest_framework import viewsets
+from .serializers import EventSerializer
+
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer  
