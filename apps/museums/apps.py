1ba0ee17e7a79f4110d7d8680b97ce67cedcd891
pedro-rohr <pedrorohr04@gmail.com>
2025-06-23 17:00:23 -0300

    Correção do nome museums

--- apps/museums/apps.py
+++ apps/museums/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class MuseumsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'museum'
+    name = 'museums'
     verbose_name = 'Museus'
